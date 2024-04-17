60d3d05dd09af367a2cbdd13200851a807ce74c1
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-04-17 20:44:50 +0500

    implementing advance filter secyion

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -5,33 +5,33 @@ import { useState } from 'react';
 import { ImLocation } from "react-icons/im";
 import React from 'react'
 import axios from 'axios';
+import { CgAddR } from "react-icons/cg";
 import toast from 'react-hot-toast';
 
 
 
 function page() {  
-  const [data,setdata]= useState([])
+  const [data,setdata]= useState()
+  const [filter,setfilter] = useState(false)
 const [title,settitle] = useState("")
 const [city,setcity] = useState("")
 const [country,setcountry] = useState("")
+// filter states  
+const [postingdate,setpostingdate] = useState("")
+const [Requirement,setRequirement] = useState("")
+const [jobtype,setjobtype] = useState("")
+const [employment,setemployment] = useState("")
 
-const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
 
   const options = {
   method: 'GET',
   url: 'https://jsearch.p.rapidapi.com/search',
   params: {
     query: `${title} in ${city}, ${country}`,
-    page: '1',
-    num_pages: '1'
+    date_posted:postingdate,
+    remote_jobs_only:jobtype,
+    employment_types:employment,
+    job_requirements:Requirement
   },
   headers: {
     'X-RapidAPI-Key': '4090cb66a7msha88ef8c2c823903p14d462jsn6331b31644fe',
@@ -41,147 +41,124 @@ const [isOpen, setIsOpen] = useState(false);
 
   const searchSubmit = (e:any)=>{
     e.preventDefault();
+
     if(!title || !country  || !city){
       toast.error('These Fields are required')
       return ;
     }
+    
     try {
       ( async function() {
       
-      // const response = await axios.request(options);
-      // setdata(response.data.data)
-    // console.log(response.data.data);
-    toast.success(options.params.query);
+      const response = await axios.request(options);
+      if (!response) {
+          alert("Something went wrong")
+      }
+      const notdata = response.data
+      const finaldata = notdata.data
+      setdata(finaldata)
+      console.log(notdata.data);
+      
+      console.log(response);
+    console.log(data);
+    console.log(options.params);
     })();
      
-    } catch (error) {
-      console.error(error);
+    } catch (error:any) {
+      throw error.message
+      // console.error(error);
     }
   }
-
+  const moreFilter = ()=>{
+    setfilter(true)
+  }
 
   return (
-    <div className='min-h-screen w-full'>
-      <div className='flex flex-col justify-center items-center w-full'>
-    <div className="search w-8/12 mt-8">
+      <div className='min-h-screen flex flex-col items-center w-full'>
+    <div className="search w-11/12  lg:w-8/12 mt-8 flex flex-col items-center ">
 
-<form onSubmit={searchSubmit} className="flex items-center space-x-2 flex-wrap">   
-
-    <div className="relative">
+<form onSubmit={searchSubmit} className="flex items-center flex-col gap-y-5 justify-center flex-wrap">   
+      <div className='flex items-center gap-x-3 gap-y-2 justify-center flex-wrap'>
+    <div className="relative w-32 sm:w-40">
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none  text-gray-500 dark:text-gray-400">
         <ImLocation size={20} />
         </div>
-        <input type="text" value={country} onChange={(e:any)=>setcountry(e.target.value)} id="voice-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Country name" />
+        <input type="text" value={country} onChange={(e:any)=>setcountry(e.target.value)} id="voice-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-md  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Country name" />
      
     </div>
-    <div className="relative">
+    <div className="relative w-36 sm:w-52">
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
         </div>
-        <input type="text" value={title} onChange={(e:any)=>settitle(e.target.value)} id="voice-search" className="rounded-md bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Job Title or Keyword" />
+        <input type="text" value={title} onChange={(e:any)=>settitle(e.target.value)} id="voice-search" className="rounded-md bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Job Title or Keyword" />
      
     </div>
-    <div className="relative">
+    <div className="relative w-32 sm:w-40">
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none  text-gray-500 dark:text-gray-400">
         <ImLocation size={20} />
         </div>
-        <input type="text" value={city} onChange={(e:any)=>setcity(e.target.value)} id="voice-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="City or State" />
+        <input type="text" value={city} onChange={(e:any)=>setcity(e.target.value)} id="voice-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs sm:text-sm rounded-md  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="City or State" />
      
     </div>
-    <button type="submit" className="rounded-lg inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-emerald-500 border border-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <svg aria-hidden="true" className="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Search
-    </button>
-
-    <div className="relative inline-block text-left">
-      <div>
-        <button
-          onClick={toggleMenu}
-          type="button"
-          className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          id="menu-button"
-          aria-expanded={isOpen ? 'true' : 'false'}
-          aria-haspopup="true"
-        >
-          Options
-          <svg
-            className="-mr-1 h-5 w-5 text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
-
-      {isOpen && (
-        <div
-          onClick={closeMenu}
-          className="fixed inset-0 z-10 w-full h-full bg-black opacity-50 cursor-default"
-        ></div>
-      )}
-
-      <div
-        className={`absolute right-0 z-20 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
-          isOpen ? 'block' : 'hidden'
-        }`}
-        role="menu"
-        aria-orientation="vertical"
-        aria-labelledby="menu-button"
-        
-      >
-        <div className="py-1" role="none">
-          <a
-            href="#"
-            className="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            
-            id="menu-item-0"
-          >
-            Account settings
-          </a>
-          <a
-            href="#"
-            className="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            
-            id="menu-item-1"
-          >
-            Support
-          </a>
-          <a
-            href="#"
-            className="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            
-            id="menu-item-2"
-          >
-            License
-          </a>
-          <form method="POST" action="#" role="none">
-            <button
-              type="submit"
-              className="text-gray-700 block w-full px-4 py-2 text-left text-sm"
-              role="menuitem"
-              
-              id="menu-item-3"
-            >
-              Sign out
-            </button>
-          </form>
-        </div>
-      </div>
     </div>
+
+    <button type="submit" className="rounded-lg w-6/12 inline-flex justify-center items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-emerald-500 border border-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg aria-hidden="true" className="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Search
+    </button>
+    {!filter?
+    <div className='w-full cursor-pointer flex justify-start  text-emerald-600'>
+    <button onClick={moreFilter}  type='button' className='flex gap-x-1 justify-start items-center'>
+    <CgAddR  size={20}/>
+      <h4 className='text-sm md:text-normal '>Add More Filters </h4>
+
+    </button>
+    </div>
+    :
+    <div className='w-full flex justify-start items-center flex-wrap gap-x-3 gap-y-2'>
+    <div className='flex flex-col'>
+    <label htmlFor="date" className='font-semibold'>Date Posted</label>
+<select id="date"  onChange={(e:any)=>setpostingdate(e.target.value)}   className='border-2 text-sm rounded-lg border-emerald-500 mt-2 outline-none'>
+  <option selected value="all">all</option>
+  <option value="today">today</option>
+  <option value="3days">3days</option>
+  <option value="week">week</option>
+  <option value="month">month</option>
+</select>
+    </div>
+    <div className='flex flex-col items-center'>
+    <label htmlFor="requirements" className='font-semibold'>Requirement</label>
+<select id="requirements" onChange={(e:any)=>setRequirement(e.target.value)}  className='border-2 text-sm rounded-lg border-emerald-500 mt-2 outline-none'>
+  <option value="">Select Experience</option>
+  <option value="under_3_years_experience">under 3 years experience</option>
+  <option value="more_than_3_years_experience">more than 3 years experience</option>
+  <option value="no_experience">no experience</option>
+  <option value="no_degree">no degree</option>
+</select>
+    </div>
+    <div className='flex flex-col'>
+    <label htmlFor="remote" className='font-semibold'>Job Type</label>
+<select id="remote"  onChange={(e:any)=>setjobtype(e.target.value)}  className='border-2 text-sm rounded-lg border-emerald-500 mt-2 outline-none'>
+  <option selected value="false">OnSite</option>
+  <option value="true">Remote</option>
+</select>
+    </div>
+    <div className='flex flex-col'>
+    <label htmlFor="employment" className='font-semibold'>Employment</label>
+<select id="employment"  onChange={(e:any)=>setemployment(e.target.value)}  className='border-2 text-sm rounded-lg border-emerald-500 mt-2 outline-none'>
+  <option value="">Select Employment</option>
+  <option value="FULLTIME">FullTime</option>
+  <option value="CONTRACTOR">Contractor</option>
+  <option value="PARTTIME">PartTime</option>
+  <option value="INTERN">Intern</option>
+</select>
+    </div>
+    </div>
+    }
 
 </form>
-  
-    </div>
-      <h1>Hi i am from job</h1>
       </div>
+  
+      <h1>Hi i am from job</h1>
     </div>
   )
 }
