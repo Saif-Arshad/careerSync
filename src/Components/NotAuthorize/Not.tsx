dcbfd16918bf318e26da9e600363259344243781
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-04-21 13:17:12 +0500

    Unauthorize page completed

---
+++ src/Components/NotAuthorize/Not.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Not.css'
+
+export default function Not() {
+  return (
+    <div>
+      <div className="circ">
+  {/* <div className="load">Unauthorized Person </div> */}
+  <div className="hands"></div>
+  <div className="body"></div>
+  <div className="head">
+    <div className="eye"></div>
+  </div>
+</div>
+    </div>
+  )
+}
--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,9 +1,30 @@
+import Not from '@/Components/NotAuthorize/Not'
 import React from 'react'
+import Link from 'next/link'
 
 function page() {
   return (
     <div className='min-h-screen'>
-      <h1>Create account first</h1>
+      <Not/>
+      <div className='flex flex-col flex-wrap items-center justify-center w-full'>
+      <div className=' md:text-xl text-gray-700 gap-y-2 flex flex-col justify-center items-center flex-wrap'><h1 className='text-xl md:text-3xl font-bold text-black block capitalize'>Unauthorized User</h1> Please create a CareerSync account to access job search.</div>
+      <div className='space-x-4 mt-10'>
+
+
+<Link
+  href="/signin"
+  className="text-white border-emerald-500 border-2 bg-emerald-500 hover:bg-emerald-700 hover:border-emerald-700 focus:ring-4 focus:ring-emerald-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
+>
+  SignIn
+</Link>
+<Link
+  href="/login"
+  className=" border-2 border-emerald-500 text-emerald-900 transition-all hover:bg-emerald-700 hover:border-emerald-700 hover:text-white focus:ring-4 focus:ring-emerald-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
+  >
+  LogIn
+</Link>
+  </div>
+      </div>
     </div>
   )
 }
